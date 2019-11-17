d015ef47776248663bfb219f2ca09bcf593a3152
hoa <hoa.le@newoceaninfosys.com>
2019-11-17 22:23:57 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor:const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                body2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                title: const TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold),
+                
+              )),
+      home: CategoriesScreen(),
+    );
+  }
+}
